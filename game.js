84d52bfbaba0507999df2cdfb3b880d022e35f71
Amirhossein Beheshti <a.h.beheshti.gh@gmail.com>
2017-08-19 18:22:16 +0430

    now it will work
    using socket.io

---
+++ game.js
@@ -0,0 +1,59 @@
+
+
+var game =  function (user1Id, user1name, user2Id, user2name, gameId , startTime) {
+
+    this.gameId = gameId;
+    this.id1 = user1Id;
+    this.id2 = user2Id;
+    this.score1 = 0;
+    this.score2 = 0;
+    this.name1 = user1name;
+    this.name2 = user2name;
+    this.isEnded1 = false;
+    this.isEnded2 = false;
+    this.startTime = startTime;
+
+    this.getName = function(id){
+        if (id == this.id1) return this.name1;
+        else if (id == this.id2) return this.name2;
+        else console.log("unknown id 1");
+    };
+
+    this.updateScore = function(id , score , callBack){
+        if (id == this.id1){
+            this.score1 = score;
+            callBack(this.id2 , this.score1 , Math.floor((Date.now() - this.startTime)/1000))
+        }
+        else if (id == this.id2){
+            this.score2 = score;
+            callBack(this.id1 , this.score2 , Math.floor((Date.now() - this.startTime)/1000))
+        }
+        else {
+            console.log("unknown id 2");
+        }
+    };
+
+    this.endGame = function(id , score , callBack){
+        if (id == this.id1){
+            this.score1 = score;
+            this.isEnded1 = true;
+        }
+        else if (id == this.id2){
+            this.score2 = score;
+            this.isEnded2 = true;
+        }
+        else {
+            console.log("unknown id 3");
+        }
+        if (this.isEnded1 == true && this.isEnded2 == true){
+            callBack(this.id1 , this.score1 , this.id2 , this.score2);
+        }
+    };
+
+    this.forceEndGame = function(callBack){
+        callBack(this.id1 , this.score1 , this.id2 , this.score2);
+    };
+
+};
+
+module.exports = game;
---
+++ index.js
@@ -0,0 +1,165 @@
+var express = require('express');
+var GameMaker = require("./game.js");
+
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+var pendingUsers = [];
+var games = {};
+var timeOfGame  = 60;
+
+app.set('port', 3001);
+server.listen(app.get('port'), function () {
+    console.log("_______SERVER IS RUNNING_______");
+});
+
+var socketStatus =
+    {
+        IDLE: "0",
+        SEARCHING: "1",
+        IN_GAME: "2",
+        GAME_ENDED: "3"
+    };
+
+io.on("connection", function(socket){
+
+    socket.gameId = null;
+    socket.status = socketStatus.IDLE;
+
+    socket.on("request_game" , function(data){
+        if (socket.status != socketStatus.IDLE) return;
+        if (checkForSameInPending(socket.id)) return; //this can be removed
+        console.log("request Game Requested");
+        var name = data.name;
+        addToPendingList(socket.id,name);
+        socket.emit("request_game_accepted");
+        socket.status = socketStatus.SEARCHING;
+        checkForConnecting();
+    });
+
+    socket.on("unrequest_game" , function(){
+        if (socket.status != socketStatus.SEARCHING) return;
+        console.log("unrequest Game Requested");
+        removeFromPendingList(socket.id);
+        socket.emit("unrequest_game_accepted");
+        socket.status = socketStatus.IDLE;
+    });
+
+    socket.on("update_game" , function(data){
+        if(socket.status != socketStatus.IN_GAME) return ;
+        // console.log("update score of player " +
+        //     games[socket.gameId].getName(socket.id) +
+        //     " with score " + data.score +
+        //     " in game with id : " + socket.gameId );
+        var score = data.score;
+        games[socket.gameId].updateScore(socket.id , score , updateScoreCallBack);
+    });
+
+    socket.on("end_game" , function(data){
+        if(socket.status != socketStatus.IN_GAME ) return ;
+        console.log("game with id " + socket.gameId + " has ended for user " +
+            games[socket.gameId].getName(socket.id) + " with score " + data.score );
+        var score = data.score;
+        games[socket.gameId].endGame(socket.id , score , endGameCallBack)
+    });
+
+    var updateScoreCallBack = function(opid , score , timePassed) {
+        if(io.sockets.connected[opid] != null)io.sockets.connected[opid].emit("update_game", {opscore : score , timepassed : timePassed});
+    };
+    
+    var endGameCallBack = function (id1 , score1 , id2 , score2) {
+        if (io.sockets.connected[id1] != null) io.sockets.connected[id1].emit("end_game" , {opscore : score2});
+        if (io.sockets.connected[id2] != null) io.sockets.connected[id2].emit("end_game" , {opscore : score1});
+        console.log("game with id " + socket.gameId + "has ended");
+        delete games[socket.gameId];
+        if (io.sockets.connected[id1] != null) io.sockets.connected[id1].status = socketStatus.IDLE;
+        if (io.sockets.connected[id1] != null) io.sockets.connected[id2].status = socketStatus.IDLE;
+    };
+
+    var checkForSameInPending = function (id) {
+        for (var i = 0; i < pendingUsers.length; i++) {
+            if (pendingUsers[i].id === id) {
+                console.log("Duplicate socketId packet found");
+                return true;
+            }
+        }
+        return false;
+    };
+
+    var addToPendingList = function(id , name) {
+        var pendingUser = new createPendingUser(id , name);
+        console.log(pendingUser.id);
+        pendingUsers.push(pendingUser);
+    };
+
+    var checkForConnecting = function() {
+        var user1;
+        var user2;
+        if (pendingUsers.length < 2) return;
+        while (pendingUsers.length > 1){
+            var temp = pendingUsers.splice(0, 1)[0];
+            if (io.sockets.connected[temp.id] != null){
+                user1 = temp;
+                break;
+            }
+        }
+        while (pendingUsers.length > 0){
+            temp = pendingUsers.splice(0, 1)[0];
+            if (io.sockets.connected[temp.id] != null){
+                user2=temp;
+                break;
+            }
+        }
+
+        if (user1 == null && user2 != null) pendingUsers.push(user2);
+        else if (user1 != null && user2 == null) pendingUsers.push(user1);
+        else if (user1 != null && user2 != null) {
+            startGame(user1 , user2);
+        }
+    };
+
+    var startGame = function(user1 , user2){
+        if(io.sockets.connected[user1.id] != null)
+            io.sockets.connected[user1.id].emit( "accept_game" , { opname : user2.name , timeofgame : timeOfGame });
+        if(io.sockets.connected[user2.id] != null)
+            io.sockets.connected[user2.id].emit( "accept_game" , { opname : user1.name , timeofgame : timeOfGame });
+        if(io.sockets.connected[user2.id] != null)
+            io.sockets.connected[user2.id].status = socketStatus.IN_GAME;
+        if(io.sockets.connected[user1.id] != null)
+            io.sockets.connected[user1.id].status = socketStatus.IN_GAME;
+        setTimeout(forceEndGame , (timeOfGame+5)*1000 );
+        var gameId = generateUniqueId();
+        games[gameId] = new GameMaker(user1.id , user1.name , user2.id , user2.name , gameId , Date.now());
+        console.log("games with id " + gameId + "has started");
+        if(io.sockets.connected[user2.id] != null)
+            io.sockets.connected[user2.id].gameId = gameId;
+        if(io.sockets.connected[user1.id] != null)
+            io.sockets.connected[user1.id].gameId = gameId;
+    };
+
+    var removeFromPendingList = function(id) {
+        for (var i = 0; i < pendingUsers.length; i++) {
+            if (pendingUsers[i].id === id) {
+                pendingUsers.splice(i,1);
+                return;
+            }
+        }
+    };
+
+    var forceEndGame = function () {
+        if (games[socket.gameId] != null){
+            games[socket.gameId].forceEndGame(endGameCallBack);
+        }
+    }
+});
+
+function createPendingUser(id , name) {
+    this.id = id;
+    this.name = name ;
+}
+
+var generateUniqueId = function () {
+    return Date.now().toString();
+};
+
